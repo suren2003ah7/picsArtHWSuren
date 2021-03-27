a66e1c96086f905353577e77345437dc8653140d
Suren Hakobyan <suren_hakobyan@aybschool.am>
2021-03-27 22:03:52 +0400

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String FullName = "Suren Hakobyan";
+        char firstLetter = 'S';
+        byte age = 17;
+        short salary = 0;
+        int favoriteNumber = 7;
+        long yob = 2003; //year of birth
+        boolean lovesPicsArt = true;
+        float height = 1.8f;
+        double mass = 63.5;
+
+        System.out.println("Full name, First letter: " + FullName + " , " + firstLetter);
+        System.out.println("age, year of birth: " + age + " , " + yob);
+        System.out.println("height, mass: " + height + " , " + mass);
+        System.out.println("salary: " + salary);
+        System.out.println("Favorite number: " + favoriteNumber);
+        System.out.println("Loves picsArt?: " + lovesPicsArt);
+        System.out.println(":)");
+    }
+}
