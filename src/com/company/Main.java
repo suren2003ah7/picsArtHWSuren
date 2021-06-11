eaa28c9bf8d538fed1e3156a15f28f2b27527179
Suren Hakobyan <surhak99@gmail.com>
2021-06-11 12:58:12 +0400

    aaa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,61 +1,31 @@
 package com.company;
 
-import com.company.project2.Action;
-
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
     public static void main(String[] args) {
-        boolean b = true;
-        Scanner sc = new Scanner(System.in);
-        while (b){
-            System.out.println("Choose");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Exit");
-            int i = sc.nextInt();
-            switch (i){
-                case 1:
-                    Action.register();
-                    break;
-                case 2:
-                    String[] user = Action.login();
-                    if (user.length == 0){
-                        break;
-                    }
-                    else{
-                        boolean bb = true;
-                        while (bb){
-                            System.out.println("You are logged in as " + user[1]);
-                            System.out.println("Choose");
-                            System.out.println("1. Print Info");
-                            System.out.println("2. Exit");
-                            int i1 = sc.nextInt();
-                            switch (i1){
-                                case 1:
-                                    System.out.println("Full Name: " + user[0]);
-                                    System.out.println("Email: " + user[2]);
-                                    break;
-                                case 2:
-                                    bb =  false;
-                                    break;
-                                default:
-                                    System.out.println("Wrong input!");
-                                    break;
-                            }
-                            }
-                        }
-                    break;
-                case 3:
-                    b = false;
-                    break;
-                default:
-                    System.out.println("Wrong input!");
-                    break;
-            }
-        }
+
     }
 }
-// HelloWorld420#
+
+// 1. A B E +
+// 2. D +
+// 3. B D E +
+// 4. A B +
+// 5. A- C D +-
+// 6 D - E
+// 7 A B E- +- C
+// 8 - B
+// 9 A C D E +
+// 10 E +
+// 11 C D +
+// 12 C D F - G
+// 13 A D +
+// 14 B - D
+// 15 - A E
+// 16 B C D +
+// 17 A E +
+// 18 C +- D E
+// 19 - B D
+// 20 C D - B E
+// 21 B - A
+// 22 B E +
+// 23 C E- +- D
